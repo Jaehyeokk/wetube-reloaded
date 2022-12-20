5fa2ca1363729b1b0152e666ed2a31a6b4acd041
Jaehyeokk <lionjk92@naver.com>
2022-12-20 09:06:16 +0900

    Using Pug

--- src/controllers/VideoController.js
+++ src/controllers/VideoController.js
@@ -1,8 +1,39 @@
-export const trending = (req, res) => res.send('Home Page Videos');
-export const search = (req, res) => res.send('Search');
-export const see = (req, res) => {
-  res.send(`See Video #ID: ${req.params.id}`);
+const fakeUser = {
+  username: 'Nicolas',
+  loggedIn: true,
 }
-export const edit = (req, res) => res.send('Edit Video');
+
+// const videos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+  {
+    title: 'Second Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+  {
+    title: 'Third Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+]
+
+export const trending = (req, res) => res.render('home', { pageTitle: 'Home', fakeUser, videos });
+export const see = (req, res) => res.render('watch', { pageTitle: 'Watch' })
+export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' });
+export const search = (req, res) => res.send('Search');
 export const upload = (req, res) => res.send('Upload Video');
 export const remove = (req, res) => res.send('remove Video');
