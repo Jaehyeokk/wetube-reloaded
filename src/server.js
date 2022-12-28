1e8944b45fb38e4005bbcc5d4420f4eb0b904bfb
jhkim <jhkim@skyware.co.kr>
2022-12-28 12:57:53 +0900

    Authentication

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
-import globalRouter from './routers/globalRouter'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import { localsMiddleware } from './middlewares'
+import rootRouter from './routers/rootRouter'
 import userRouter from './routers/userRouter'
 import videoRouter from './routers/videoRouter'
 
@@ -13,8 +16,16 @@ app.set('views', process.cwd() + '/src/views')
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.DB_URL })
+}))
 
-app.use('/', globalRouter);
+app.use(localsMiddleware)
+
+app.use('/', rootRouter);
 app.use('/users', userRouter)
 app.use('/videos', videoRouter);
 
