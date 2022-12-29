1bcb921979b903202cc084bbc16d9fcf0682cb8e
jhkim <jhkim@skyware.co.kr>
2022-12-29 11:04:22 +0900

    Setup

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.DB_URL)
+
+const db = mongoose.connection
+
+const handleError = (error) => console.log('❌ DB Error', error)
+const handleOpen = () => console.log('✅ Connected to DB')
+
+db.on('error', handleError)
+db.once('open', handleOpen)
---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import morgan from 'morgan'
+import rootRouter from './routers/rootRouter'
+import userRouter from './routers/userRouter'
+import videoRouter from './routers/videoRouter'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.set('view engine', 'pug')
+app.set('views', `${process.cwd()}/src/views`)
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  secret: process.env.DB_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ 
+    mongoUrl: process.env.DB_URL
+  })
+}))
+
+app.use('/', rootRouter)
+app.use('/users', userRouter)
+app.use('/videos', videoRouter)
+
+export default app
