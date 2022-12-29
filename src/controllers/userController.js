f939c3f7596dae8708181e7ca668c089d2b02a60
jhkim <jhkim@skyware.co.kr>
2022-12-29 09:36:43 +0900

    Initializing for review

--- src/controllers/userController.js
+++
@@ -1,6 +0,0 @@
-export const join = (req, res) => res.send('join');
-export const login = (req, res) => res.send('login')
-export const logout = (req, res) => res.send('Logout')
-export const see = (req, res) => res.send('See')
-export const edit = (req, res) => res.send('Edit User');
-export const remove = (req, res) => res.send('Remove User');
